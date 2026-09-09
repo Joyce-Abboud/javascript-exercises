194010844dbf7cd28f178a92b8ae1d292b9e04ee
Joyce-Abboud <Joyce.abboud2025@gmail.com>
2026-09-09 15:45:30 +0300

    finihsed the ex repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times < 0) return "ERROR"; //the only thing that i took 
+    let string= "";
+    for(let i =0; i < times; i ++){
+        string +=str;
+    }
+    return string;
 };
 
 // Do not edit below this line

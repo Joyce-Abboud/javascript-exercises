005456b6dffd9ce0f21961ed62e6ca48374eda79
Joyce-Abboud <Joyce.abboud2025@gmail.com>
2026-09-10 23:39:18 +0300

    still have soome test to pass in removeFromArray, but i just finished summAll exercice

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+let arr = [1, 2, 3, 4];
+const removeFromArray = function(arr, ...theArgs) {
+    for(let i =0; i<theArgs.length; i++){
+        //if i is found in arr we have to remove it- for every i found
+        //should i use loop or mapp?
+        const index = arr.indexOf(theArgs[i]);
+
+        if (index !== -1) { 
+        // 2. Remove it if found (1 means remove exactly 1 item)
+        arr.splice(index, 1); 
+        //else ignore it 
+
+        //types??
+    }
+    return arr;
+}    
 };
 
 // Do not edit below this line

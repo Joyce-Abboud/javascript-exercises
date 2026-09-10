bd9f0b5290154a96159f2de7898c0a78bbba6cc0
Joyce-Abboud <Joyce.abboud2025@gmail.com>
2026-09-11 00:01:03 +0300

    finished evry test, it was missing a loop and a correct position of return.figuerd it on my own

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -2,18 +2,19 @@ let arr = [1, 2, 3, 4];
 const removeFromArray = function(arr, ...theArgs) {
     for(let i =0; i<theArgs.length; i++){
         //if i is found in arr we have to remove it- for every i found
-        //should i use loop or mapp?
         const index = arr.indexOf(theArgs[i]);
+        for (let j=0; j< arr.length; j++){
+            const index = arr.indexOf(theArgs[i]);
+            if (index !== -1) { 
+            //find each one of this index so filter
 
-        if (index !== -1) { 
-        // 2. Remove it if found (1 means remove exactly 1 item)
-        arr.splice(index, 1); 
-        //else ignore it 
-
-        //types??
-    }
-    return arr;
-}    
+            // 2. Remove it if found (1 means remove exactly 1 item)
+            arr.splice(index, 1); 
+        }
+        }
+        
+    
+    }return arr;
 };
 
 // Do not edit below this line

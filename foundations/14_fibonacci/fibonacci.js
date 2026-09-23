f6ababf72a54bd0aaffd44ce9c0d04c3102722d3
Joyce-Abboud <Joyce.abboud2025@gmail.com>
2026-09-23 15:46:48 +0300

    fibonnaci, used an approach that calls it self uuntil n<2

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+let total = 0;
+const fibonacci = function (n) {
+    n = parseInt(n);
+    if (n < 0) {
+        return "OOPS";
+    } else if (n < 2) {
+        total += n;
+        return n;
+    } else {
+        return (fibonacci(n - 1) + fibonacci(n - 2));
+    }
 };
 
 // Do not edit below this line

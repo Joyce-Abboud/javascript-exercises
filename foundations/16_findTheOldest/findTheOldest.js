6cd4dbbfe8b65db8e6559d5b7dce3ac20d7a3f36
Joyce-Abboud <Joyce.abboud2025@gmail.com>
2026-09-23 20:58:37 +0300

    finished find the oldest - not very proud

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const currentAges = people.map((person) => {
+        const age = person.yearOfDeath - person.yearOfBirth;
+        return age;
+    });
+    if (people.some(person => person.yearOfDeath === undefined)) {
+        currentAges.forEach((age, index) => {
+            if (people[index].yearOfDeath === undefined) {
+                currentAges[index] = ((new Date()).getFullYear()) - people[index].yearOfBirth;
+            }
+        });
+    }
 
+    const peopleWithAges = people.map((person, index) => {
+        return { name: person, age: currentAges[index] };
+    });
+
+
+    const sorted = peopleWithAges.sort((a, b) => b.age - a.age);
+
+    // Returns the person object from the highest-aged pair
+    return sorted[0].name;
 };
 
 // Do not edit below this line

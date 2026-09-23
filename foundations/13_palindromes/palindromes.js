0cb5baa845ed79815771fc086d60b38b3ad41fb5
Joyce-Abboud <Joyce.abboud2025@gmail.com>
2026-09-23 15:06:36 +0300

    done wwith the palindrom but had some triuble with thw /[^\w]/g and the /[^\w\s]/g, cause of the spaces

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const text = str.toLowerCase().replace(/[^\w]/g, "").trim();
+    const reversed = text.split("").reverse().join("");
+    if (reversed === text) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
